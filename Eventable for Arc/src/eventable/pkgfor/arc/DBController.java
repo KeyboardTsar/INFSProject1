946aebfe08af69a5817f6f205537a8efb6caceb2
DESKTOP-UUNU78F\Ajay Matta <ajaymatta2006@gmail.com>
2018-05-03 11:51:12 +1000

    Updated DBController.java

--- Eventable for Arc/src/eventable/pkgfor/arc/DBController.java
+++ Eventable for Arc/src/eventable/pkgfor/arc/DBController.java
@@ -19,8 +19,6 @@ public class DBController {
             try {
                 Class.forName("oracle.jdbc.driver.OracleDriver");
                 conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","hr","hr");
-                Statement st=con.createStatement();
-                
                 }
             } catch (SQLException ex) {
                 ex.printStackTrace();
